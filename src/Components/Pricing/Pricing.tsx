3dd857f8f828cb5fefee3b3f026f724bc11e77cb
joshutik <utik2.4real@gmail.com>
2026-01-19 20:48:55 +0100

    fininshed the pricing table

--- src/Components/Pricing/Pricing.tsx
+++ src/Components/Pricing/Pricing.tsx
@@ -18,13 +18,13 @@ const Pricing = () => {
             </div>
 
             <div className="container">
-                <div className="row align-items-center my-5 justify-content-center">
+                <div className="row my-5 justify-content-center">
                     <div className="col-lg-4 col-md-6 col-sm-12 mb-3 mb-sm-3">
-                        <div className="card border-0 h-100 shadow-lg pricing-card text-start pb-5">
+                        <div className="card border-0 h-100 rounded-5 shadow-lg pricing-card text-start pb-5 px-3">
                             <div className="card-body">
-                                <h5 className="card-title fw-bold">Free</h5>
+                                <h5 className="card-title fw-bold text-start my-3">Free</h5>
                                 <span className='fw-bold fs-3'>₦ 0 <small className='fs-5 fw-normal'>/forever</small> </span>
-                                <p>
+                                <p className="my-3">
                                     Start learning with limited daily
                                     access
                                 </p>
@@ -42,16 +42,50 @@ const Pricing = () => {
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12 mb-3 mb-sm-3">
-                        <div className="card h-100 shadow-lg pricing-card text-center">
-                            <div className="card-body">
-                                <h5 className="card-title fw-bold">Pro Plan</h5>
+                        <div className="card h-100 shadow-lg pricing-card text-center bg-primary text-light rounded-5 pb-5 px-3">
+                            <div className="card-body text-start">
+                                <h5 className="card-title fw-bold my-3">Student</h5>
+                                <span className='fw-bold fs-3 py-3'>₦ 2,500<small className='fs-5 fw-normal'> /per month</small> </span>
+                                <p className="my-3">
+                                    Everything you need to ace your
+                                    exams
+                                </p>
+
+                                <ul className='text-start w-100  my-4'>
+                                    <li className='list-group-item my-3 text-start'><i className="bi avialable bi-check2"></i>Unlimited practice questions</li>
+                                    <li className='list-group-item my-3 text-start'><i className="bi avialable bi-check2"></i> Full AI tutor</li>
+                                    <li className='list-group-item my-3 text-start'><i className="bi avialable bi-check2"></i>All subject included</li>
+                                    <li className='list-group-item my-3 text-start'><i className="bi avialable bi-check2"></i>Detailed analytics</li>
+                                    <li className='list-group-item my-3 text-start'><i className="bi avialable bi-check2"></i>Mock test & CBT practice</li>
+                                    <li className='list-group-item my-3 text-start'><i className="bi avialable bi-check2"></i>Offline downloads</li>
+                                    <li className='list-group-item my-3 text-start'><i className="bi avialable bi-check2"></i>Priority support</li>
+                                </ul>
+                                <div>
+                                    <button className="btn btn-light w-100">Start Free Trial</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12 mb-3 mb-sm-3">
-                        <div className="card h-100 shadow-lg pricing-card text-center">
+                        <div className="card border-0 h-100 shadow-lg pricing-card rounded-5 text-start pb-5 px-3">
                             <div className="card-body">
-                                <h5 className="card-title fw-bold">Enterprise Plan</h5>
+                                <h5 className="card-title fw-bold text-start my-3">Family</h5>
+                                <span className='fw-bold fs-3 py-3'>₦ 5,000 <small className='fs-5 fw-normal'>/per month</small> </span>
+                                <p className="my-3">
+                                    Perfect for multiple students in one
+                                    family
+                                </p>
+
+                                <ul className='text-start w-100  my-4'>
+                                    <li className='list-group-item my-3 text-start'><i className="bi avialable bi-check2"></i>Everything in student plan</li>
+                                    <li className='list-group-item my-3 text-start'><i className="bi avialable bi-check2"></i> Up to 4 student accounts</li>
+                                    <li className='list-group-item my-3 text-start'><i className="bi avialable bi-check2"></i>Parent dashboard</li>
+                                    <li className='list-group-item my-3 text-start'><i className="bi avialable bi-check2"></i>Family progress reports</li>
+                                    <li className='list-group-item my-3 text-start'><i className="bi avialable bi-check2"></i>Dedicated support</li>
+                                </ul>
+                                <div>
+                                    <button className="btn btn-dark w-100">Get Family Plan</button>
+                                </div>
                             </div>
                         </div>
                     </div>
